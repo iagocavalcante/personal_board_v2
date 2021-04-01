3e7665c998f1f42c27e3e233f27796d647bdcd54
Iago Cavalcante <iagoangelimc@gmail.com>
2021-04-01 00:45:47 -0300

    ui: create custom styles for app

--- lib/personal_board_v2_web/live/components/card_live.ex
+++ lib/personal_board_v2_web/live/components/card_live.ex
@@ -5,9 +5,9 @@ defmodule PersonalBoardV2Web.CardComponent do
     editing? = assigns.edit_card_title == assigns.card.id
 
     ~L"""
-    <div class="list-card" data-card-id="<%= @card.id %>" data-list-id="<%= @list_id %>" >
-      <div  phx-click="archive_card" phx-value-card_id="<%= @card.id %>">
-        <i class="material-icons delete-card">delete</i>
+    <div class="card" data-card-id="<%= @card.id %>" data-list-id="<%= @list_id %>" >
+      <div class="icons" >
+        <i class="material-icons options-card">more_horiz</i>
       </div>
       <textarea style="display:<%= if editing?, do: "block", else: "none" %>" autofocus
         name="card-title"  phx-hook="CardTitle" value="<%= @card.title %>" id="<%= @card.id %>" data-card_id="<%= @card.id %>"><%= @card.title %></textarea>
